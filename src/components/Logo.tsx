0785842c88dab591c794710967dba96031f07b29
kriegs01 <alacapa47@gmail.com>
2026-05-05 11:59:17 +0000

    logo fix 2

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Logo: React.FC<{ className?: string }> = ({ className = '' }) => {
   return (
-    <div className={`flex items-center tracking-tight text-indigo-600 dark:text-indigo-400 drop-shadow-[3px_3px_0_rgba(244,114,182,1)] cursor-pointer group ${className}`}>
+    <div className={`flex items-center tracking-tight bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent drop-shadow-[3px_3px_0_#d1d5db] dark:drop-shadow-[3px_3px_0_#ffffff] cursor-pointer group ${className}`}>
       <span className="font-titan transition-transform duration-300 group-hover:-translate-y-2 rotate-[-6deg]">B</span>
       <span className="font-titan transition-transform duration-300 group-hover:-translate-y-2 group-hover:delay-75 translate-y-0.5 rotate-[3deg]">u</span>
       <span className="font-titan transition-transform duration-300 group-hover:-translate-y-2 group-hover:delay-100 -translate-y-0.5 rotate-[-4deg]">d</span>
